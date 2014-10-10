2820aef361807638cb879e0620bf780524ea09c5
gustavo.delgiudice <gustavo.delgiudice@etermax.com>
2014-10-10 17:46:11 -0300

    First attempt at implementing film details controller.

--- TPEApp/src/main/java/ar/edu/itba/grupo2/web/FilmController.java
+++ TPEApp/src/main/java/ar/edu/itba/grupo2/web/FilmController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.itba.grupo2.dao.exceptions.FilmNotFoundException;
+import ar.edu.itba.grupo2.model.Comment;
 import ar.edu.itba.grupo2.model.Film;
+import ar.edu.itba.grupo2.model.User;
 import ar.edu.itba.grupo2.service.FilmService;
 import ar.edu.itba.grupo2.service.impl.FilmServiceImpl;
 
@@ -29,6 +33,35 @@ public class FilmController {
 				filmService.filterNewReleases(filmList, 7));
 		
 		mav.setViewName("welcome");
+		
+		return mav;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView filmDetails(@RequestParam(value = "id", required=false) Integer id) {
+		ModelAndView mav = new ModelAndView();
+		
+		FilmService filmService = FilmServiceImpl.getInstance();
+		Film film = null;
+		try {
+			film = filmService.getFilmById(id);
+			
+			//User user = (User)req.getSession(false).getAttribute("user");
+			
+			final List<Comment> commentList = filmService.getCommentsForFilm(film);
+			
+			mav.addObject("commentList", commentList);
+			mav.addObject("film", film);
+			
+			/*if(user != null) {
+				boolean userCanComment = filmService.userCanComment(film, user);
+				mav.addObject("userCanComment", userCanComment);
+			}*/
+		} catch (FilmNotFoundException e) {
+			film = null;
+		}
+		
+		mav.setViewName("filmDetails");
 		
 		return mav;
 	}
