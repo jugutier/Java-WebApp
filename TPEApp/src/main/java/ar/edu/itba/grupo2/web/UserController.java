8a805b13240f8f1aec96edbce81d5fd63cd7ffee
Julian Gutierrez <julian.3.1@gmail.com>
2014-10-22 17:20:24 -0300

    More migration

--- TPEApp/src/main/java/ar/edu/itba/grupo2/web/UserController.java
+++ TPEApp/src/main/java/ar/edu/itba/grupo2/web/UserController.java
@@ -135,7 +135,7 @@ public class UserController {
 		String ret = null;
 		
 		if (fromPage == null) {
-			fromPage = "home";
+			fromPage = "welcome";
 		}
 		
 		loggedUser = userRepo.logIn(email, password);
@@ -169,7 +169,94 @@ public class UserController {
 		ModelAndView mav = new ModelAndView();
 		
 		mav.setViewName("resetPassword");
+		/*UserManager userManager = new UserManagerImpl((HttpServletRequest)req);
+		userManager.resetUser();
 		
+		resp.sendRedirect(resp.encodeRedirectURL("welcome"));*/
 		return mav;
 	}
 }
+
+/*UserService userService = UserServiceImpl.getInstance();
+String email = req.getParameter("email");
+String password = req.getParameter("password");
+String passwordConfirm = req.getParameter("passwordConfirm");
+String answer = req.getParameter("answer");
+String stage = req.getParameter("stage");
+
+List<String> errors = new ArrayList<String>();
+
+if (stage != null && stage.compareTo("getEmail") == 0) {
+	errors.addAll(validateEmailStage(email));
+	
+	if (errors.isEmpty()) {
+		// Display Secret Question
+		req.setAttribute("email", email);
+		req.setAttribute("secretQuestion", userService.getSecretQuestionForEmail(email));
+		req.setAttribute("stage", "question");
+	}
+}
+else if (stage != null && stage.compareTo("question") == 0){
+	errors.addAll(validateAnswerStage(email, answer, password, passwordConfirm));
+	
+	if (errors.isEmpty()) {
+		resp.sendRedirect(resp.encodeRedirectURL("home"));
+		return;
+	}
+}
+else {
+	doGet(req, resp);
+}
+
+if (!errors.isEmpty()) {
+	req.setAttribute("errors", errors);
+	doGet(req, resp);
+}
+else {
+	req.getRequestDispatcher("/WEB-INF/jsp/resetPassword.jsp").forward(req, resp);
+}
+}*/
+
+
+/*private List<String> validateEmailStage(String email) {
+List<String> errors = new ArrayList<String>();
+UserService userService = UserServiceImpl.getInstance();
+
+if (ValidationUtilities.paramEmpty(email)) {
+	errors.add("noMail");
+}
+else if (!ValidationUtilities.isEmail(email)) {
+	errors.add("invalidMail");
+}
+else if (!userService.existsUser(email)){
+	errors.add("wrongMail");
+}
+
+return errors;
+}
+
+private List<String> validateAnswerStage(String email, String answer,
+	String password, String passwordConfirm) {
+List<String> errors = new ArrayList<String>();
+UserService userService = UserServiceImpl.getInstance();
+
+errors.addAll(validateEmailStage(email));
+
+if (ValidationUtilities.paramEmpty(answer)) {
+	errors.add("noAnswer");
+}
+else if (!userService.resetPasswordForEmail(email, passwordConfirm, answer)) {
+	errors.add("wrongAnswer");
+}
+if (ValidationUtilities.paramEmpty(password)) {
+	errors.add("noPassword");
+}
+if (ValidationUtilities.paramEmpty(passwordConfirm)) {
+	errors.add("noPasswordConfirm");
+}
+if (password.compareTo(passwordConfirm) != 0) {
+	errors.add("noCoincidence");
+}
+
+return errors;
+}*/
