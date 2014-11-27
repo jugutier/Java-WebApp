e671d42dc0919861aacc2540aa3808a170f89df9
gustavo.delgiudice <gustavo.delgiudice@etermax.com>
2014-11-27 09:08:08 -0300

    Controlled visibility of components in FilmCommentListItem

--- TPEApp/src/main/java/ar/edu/itba/grupo2/web/widget/comment/FilmCommentListItem.java
+++ TPEApp/src/main/java/ar/edu/itba/grupo2/web/widget/comment/FilmCommentListItem.java
@@ -1,15 +1,23 @@
 package ar.edu.itba.grupo2.web.widget.comment;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import ar.edu.itba.grupo2.domain.comment.Comment;
 import ar.edu.itba.grupo2.domain.user.User;
+import ar.edu.itba.grupo2.domain.user.UserRepo;
+import ar.edu.itba.grupo2.web.GAJAmdbSession;
 import ar.edu.itba.grupo2.web.widget.StarScoreIndicator;
 
 public class FilmCommentListItem extends Panel {
 
+	@SpringBean
+	private UserRepo users;
+	
 	public FilmCommentListItem(String id, Comment comment) {
 		super(id);
 		
@@ -24,10 +32,35 @@ public class FilmCommentListItem extends Panel {
 			
 		};
 		
+		Link<Void> reportButton = new Link<Void>("report-button") {
+
+			@Override
+			public void onClick() {
+				// TODO Report this comment
+			}
+			
+		};
+		
+		Link<Void> deleteButton = new Link<Void>("delete-button") {
+
+			@Override
+			public void onClick() {
+				// TODO Delete this comment
+				
+			}
+			
+		};
+		
+		Form ratingForm = new Form<>("comment-rate-form");
+		
 		User user = comment.getUser();
+		User loggedInUser = GAJAmdbSession.get().getLoggedInUser(users);
 		
 		vipTag.setVisible(user.isVip());
 		adminTag.setVisible(user.isAdmin());
+		reportButton.setVisible(loggedInUser != null);
+		deleteButton.setVisible(loggedInUser != null && loggedInUser.isAdmin());
+		ratingForm.setVisible(loggedInUser != null);
 		
 		add(usernameLink);
 		usernameLink.add(new Label("username", comment.getUser().getName()));
@@ -35,6 +68,9 @@ public class FilmCommentListItem extends Panel {
 		add(new Label("message", comment.getText()));
 		add(adminTag);
 		add(vipTag);
+		add(reportButton);
+		add(deleteButton);
+		add(ratingForm);
 	}
 
 }
