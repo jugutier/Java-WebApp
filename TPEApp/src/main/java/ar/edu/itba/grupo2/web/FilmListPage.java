f2e9a17cc9c86b738b7c2406361c0471634900aa
Gustavo Del Giudice <gdelgiud@itba.edu.ar>
2014-12-01 15:30:18 -0300

    Implemented Film filtering.

--- TPEApp/src/main/java/ar/edu/itba/grupo2/web/FilmListPage.java
+++ TPEApp/src/main/java/ar/edu/itba/grupo2/web/FilmListPage.java
@@ -2,35 +2,76 @@ package ar.edu.itba.grupo2.web;
 
 import java.util.List;
 
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 
 import ar.edu.itba.grupo2.domain.film.Film;
+import ar.edu.itba.grupo2.domain.genre.Genre;
 import ar.edu.itba.grupo2.web.widget.film.NormalFilmListItem;
 
 @SuppressWarnings("serial")
 public class FilmListPage extends BasePage {
 	
+	private transient Genre genre;
+	private transient String director;
+	
 	public FilmListPage() {
 		super();
 		
+		loadFilterForm();
 		loadFilmList();
+	}
+	
+	private void loadFilterForm() {
+		Form<FilmListPage> form = new Form<FilmListPage>("form", new CompoundPropertyModel<FilmListPage>(this)) {
+			@Override
+			protected void onSubmit() {
+				GAJAmdbSession session = GAJAmdbSession.get();
+				
+				if (!session.isLoggedIn()) {
+					director = null;
+				}
+			}
+		};
+		
+		TextField<String> directorText = new TextField<String>("director") {
+			@Override
+			public boolean isVisible() {
+				return GAJAmdbSession.get().isLoggedIn();
+			}
+		};
+		
+		DropDownChoice<Genre> genreDropDown = new DropDownChoice<Genre>("genre", new LoadableDetachableModel<List<Genre>>() {
+			
+			@Override
+			protected List<Genre> load() {
+				return films.getGenres();
+			}
+		});
+		
+		form.add(directorText);
+		form.add(genreDropDown);
+		add(form);
 	}
 	
 	private void loadFilmList() {
 		final IModel<List<Film>> filmListModel = new LoadableDetachableModel<List<Film>>() {
 			@Override
 			protected List<Film> load() {
-				return films.getAll();
+				return films.getFiltered(genre, director);
 			}
 		};
 		
-		add(new ListView<Film>("filmList", filmListModel) {
+		add(new ListView<Film>("film", filmListModel) {
 			@Override
 			protected void populateItem(ListItem<Film> item) {
-				item.add(new NormalFilmListItem("filmItem", item.getModel(), filmListModel));
+				item.add(new NormalFilmListItem("filmPanel", item.getModel(), filmListModel));
 			}
 		});
 	}
