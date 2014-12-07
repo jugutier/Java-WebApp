74d039ad0a9d62dd5fa6b1effdcea72b60baa390
Julian Gutierrez <julian.3.1@gmail.com>
2014-12-07 13:15:16 -0300

    migration onetoone

--- TPEApp/src/test/resources/sprint3/migrate.sql
+++ TPEApp/src/test/resources/sprint3/migrate.sql
@@ -1,5 +1,6 @@
 alter table film add column image_fk int;
 alter table film add foreign key (image_fk) references image(id);
+insert into film (image_fk) (select image.id from film, image where film.id = image.film_id);
 alter table image drop column film_id;
 
 alter table film add column visits int;
